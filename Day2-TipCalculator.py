9fd69c539593afc0025c2b2d167a00f669cf1c2d
Mirza Mohammed Baig <104741210+M1RZAB4IG@users.noreply.github.com>
2024-05-29 13:50:13 -0400

    Simplify Day2-TipCalculator.py

--- Day2-TipCalculator.py
+++ Day2-TipCalculator.py
@@ -8,22 +8,19 @@
 #Write your code below this line 👇
 
 print("Welcome to the tip calculator!")
-total_bill = input("What was the total bill? $")
-tip_percentage = input(
-    "What percentage tip would you like to give? 10, 12, or 15? ")
-people_split = input("How many people to split the bill? ")
+total_bill = float(input("What was the total bill? $"))
+tip_percentage = float(input(
+    "What percentage tip would you like to give? 10, 12, or 15? "))
+people_split = float(input("How many people to split the bill? "))
 
-total_bill_float = float(total_bill)
-tip_percentage_float = float(tip_percentage)
-people_split_float = float(people_split)
 
-tip_percentage_float_final = (tip_percentage_float / 100) + 1
-total_bill_float_final = total_bill_float * tip_percentage_float_final
-total_bill_float_final_split = total_bill_float_final / people_split_float
-total_bill_float_final_split_rounded = "{:.2f}".format(
-    (total_bill_float_final_split))
+tip_percentage_float_final = (tip_percentage / 100) + 1
+total_bill_float_final = total_bill * tip_percentage_float_final
+total_bill_float_final_split = total_bill_float_final / people_split
 
-print(f"Each person should pay: $ {total_bill_float_final_split_rounded}")
+
+print(f"Each person should pay: $ {total_bill_float_final_split:.2f}")
+
 """"# Sample Input
 print("Welcome to the tip calculator!")
 total_bill = input("What was the total bill? $124.56")
@@ -32,4 +29,3 @@ people_split = input("How many people to split the bill? 7")
 
 # Sample Output
 Each person should pay: $19.93 """
-
